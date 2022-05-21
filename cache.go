1bc2d7cc662a9a76883ec00c754d93f4cf8a934d
Gogi <Gogi22@users.noreply.github.com>
2022-05-21 12:35:16 +0400

    finished cache

--- cache.go
+++ cache.go
@@ -2,22 +2,56 @@ package cache
 
 import "time"
 
+type Value struct {
+	val      string
+	deadline time.Time
+}
+
 type Cache struct {
+	dict map[string]Value
 }
 
 func NewCache() Cache {
-	return Cache{}
+	c := Cache{dict: make(map[string]Value)}
+	return c
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c Cache) Get(key string) (string, bool) {
+	v, ok := c.dict[key]
 
+	if !ok {
+		return "", false
+	}
+
+	if !c.isValid(key) {
+		delete(c.dict, key)
+		return "", false
+	}
+
+	return v.val, true
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.dict[key] = Value{val: value}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	keys := make([]string, 0, len(c.dict))
+	for key := range c.dict {
+		if !c.isValid(key) {
+			delete(c.dict, key)
+		} else {
+			keys = append(keys, key)
+		}
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.dict[key] = Value{val: value, deadline: deadline}
+}
+
+func (c Cache) isValid(key string) bool {
+	return c.dict[key].deadline.Equal(time.Time{}) || c.dict[key].deadline.After(time.Now())
 }
